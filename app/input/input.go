bb7f5fa860fe225eda54cf0c0f501eeb5f39cd09
Drew <muregiandrew@gmail.com>
2026-09-04 13:27:01 +0300

    input: standardized user promts into a struct

--- app/input/input.go
+++ app/input/input.go
@@ -6,21 +6,31 @@ import (
 	"strings"
 )
 
-var commandList map[string]bool = map[string]bool{
-	"exit": true,
+type Prompt struct {
+	Command string
+	Arg     string
 }
 
-// Get User Input from Standard Input
-func GetInput() (string, error) {
+// Get User Prompt from Standard Input
+func GetPrompt() (*Prompt, error) {
 	reader := bufio.NewReader(os.Stdin)
-	command, err := reader.ReadString('\n')
+	promptStr, err := reader.ReadString('\n')
 
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-	return strings.TrimSpace(command), err
-}
 
-func IsCommand(command string) bool {
-	return commandList[command]
+	promptStr = strings.TrimSpace(promptStr)
+
+	argSplit := strings.SplitN(promptStr, " ", 2)
+	command := argSplit[0]
+	arg := ""
+	if len(argSplit) > 1{
+		arg = argSplit[1]
+	}
+
+	return &Prompt{
+		Command: command,
+		Arg: arg,
+	}, nil
 }
