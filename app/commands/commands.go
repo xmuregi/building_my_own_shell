8b4270b8a46b6dafc8caef6a6e5d8f73627b8fc9
Drew <muregiandrew@gmail.com>
2026-09-05 10:28:05 +0300

    input: add comments about the functions

--- app/commands/commands.go
+++ app/commands/commands.go
@@ -1,12 +1,14 @@
 package commands
 
 
-
+// Allowed Shell builtins
 var commandList map[string]bool = map[string]bool{
 	"exit": true,
 	"echo": true,
+	"type": true,
 }
 
-func IsCommand(command string) (bool, error) {
-	return commandList[command], nil
+// Returns bool value for Shell builtins
+func IsCommand(command string) (bool) {
+	return commandList[command]
 }
